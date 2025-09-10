b61684a575001b554dfb5119ea270fa7ab4f8cc3
huyya <s7nohito@gmail.com>
2025-09-10 12:07:25 +0900

    AirFlow
    AirFlowをとりあえず完成

---
+++ Assets/Scripts/AirFlowGenerator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirFlowGenerator : MonoBehaviour
+{
+    public GameObject AirFlow;
+    private Vector3 clickPosition;
+    public float AirFlowPower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            AirFlowPower = Input.mousePosition.y - clickPosition.y;
+            Debug.Log(AirFlowPower);
+            Debug.Log(clickPosition);
+            clickPosition.y = 0;
+            clickPosition.z = 100;
+            Instantiate(AirFlow, Camera.main.ScreenToWorldPoint(clickPosition), AirFlow.transform.rotation);
+        }
+    }
+}
